29974e0a15928ddf3bf4c43ff5c1696c4491f478
abj360 <oluwatoyosi.abolaji25@gmail.com>
2026-04-03 10:21:00 +0000

    test(extract): add unit tests for extraction pipeline

---
+++ tests/unit/conftest.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+conftest.py --- shared fixtures for unit tests
+
+Contains:
+"""
+
+import pytest
+
+from extract.schema import Triple
+from tests.unit.factories import make_triple
