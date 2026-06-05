4ff8f52c61db29526b4f912240c4906b9b213f2e
Yannick-David314 <2025.yannick.udechukwu@gmail.com>
2026-06-05 11:28:00 +0000

    fixup: address review comments

---
+++ viz/src/components/GraphLegend.jsx
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+/**
+ * GraphLegend.jsx --- legend mapping entity types to their palette colors
+ *  *
+ *  * Contains:
+ */
+
+import { colorForType } from "../utils/graphStyles.js";
