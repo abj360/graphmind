28a1657cda44ec762e3a7c9ba4ae3a73c08559ff
Yannick-David314 <2025.yannick.udechukwu@gmail.com>
2026-06-05 11:32:00 +0000

    style: wrap long lines

--- viz/src/components/GraphLegend.jsx
+++ viz/src/components/GraphLegend.jsx
@@ -3,6 +3,30 @@
  * GraphLegend.jsx --- legend mapping entity types to their palette colors
  *  *
  *  * Contains:
+ *  *   GraphLegend: type-to-color legend overlay
  */
 
 import { colorForType } from "../utils/graphStyles.js";
+
+/**
+ * Renders the legend mapping entity types to palette colors.
+ *
+ * @param props.nodes - Visible nodes used to list present types.
+ * @returns element - Legend overlay listing present types.
+ */
+export default function GraphLegend({ nodes }) {
+  const types = [...new Set(nodes.map((node) => node.type))].sort();
+  if (types.length === 0) {
+    return null;
+  }
+  return (
+    <ul className="graph-legend">
+      {types.map((type) => (
+        <li key={type}>
+          <span className="legend-swatch" style={{ backgroundColor: colorForType(type) }} />
+          {type}
+        </li>
+      ))}
+    </ul>
+  );
+}
